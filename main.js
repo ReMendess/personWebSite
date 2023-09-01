057a09d7fce3e09876f4c4686ed9196e8e6b484f
ReMendess <renan_oliveira_99@hotmail.com>
2023-09-01 18:32:17 -0300

    uptadeTeclado

--- main.js
+++ main.js
@@ -1,7 +1,39 @@
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
 
+function tocaSom (idElementoAudio) {
+
+    document.querySelector(idElementoAudio).play();
 }
 
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+// selecionando todos os .teclas e criando uma lista de referencia.
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+
+
+
+for ( let contador = 0; contador < listaDeTeclas.length; contador++){
+
+
+
+    const tecla =  listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function() {
+        tocaSom(idAudio);
+    }
+    
+   /* tecla.onkeydown = function(){
+        if (){
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function(){
+        tecla.classList.remove('ativa');
+    }
+
+*/
+    
+}
+
